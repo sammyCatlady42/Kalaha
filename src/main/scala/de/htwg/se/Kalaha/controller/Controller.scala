6de52d61e043fc36c03d7ff981ddbd69a31036c2
Creliv <burger.simon@gmx.de>
2019-01-18 20:30:01 +0100

    finished most mechanics

--- src/main/scala/de/htwg/se/Kalaha/controller/Controller.scala
+++ src/main/scala/de/htwg/se/Kalaha/controller/Controller.scala
@@ -22,32 +22,69 @@ class Controller {
 
 
   def move(index: Int): Unit = {
-    print("index = " + index + "\n")
-    print("Turn = " + round % 2 + "\n")
+    //print("index = " + index + "\n")
+    //print("Turn = " + round % 2 + "\n")
     var idx = index
+    var last = 0
     board.oldgb = board.gameboard.clone()
-    val countStonesInMuld: Int = board.gameboard(index)
-    board.gameboard(index) = 0
+    val countStonesInMuld: Int = board.gameboard(idx)
+    //print("Balls = " + countStonesInMuld + "\n")
+    board.gameboard(idx) = 0
     for (i <- 1 until countStonesInMuld + 1) {
-      if ((round % 2 == 0 && (index + i) % 14 == 0) || (round % 2 == 1 && (index + i) % 14 == 7)) {
+      if ((round % 2 == 0 && (idx + i) % 14 == 0) || (round % 2 == 1 && (idx + i) % 14 == 7)) {
         //print("turn: " + round % 2 + " i = " + (index + i) + " x = " + countStonesInMuld + " skip\n")
-        if (index + i >= board.gameboard.length) {
-          val y: Int = index + countStonesInMuld - board.gameboard.length
+        if (idx + i >= board.gameboard.length) {
+          val y: Int = (idx + i - board.gameboard.length) % 14
           board.gameboard(y + 1) += 1
+          idx += 1
         } else {
-          board.gameboard(index + countStonesInMuld) += 1
+          board.gameboard(idx + i) += 1
         }
       } else {
-        if (index + i >= board.gameboard.length) {
-          val y: Int = index + i - board.gameboard.length
+        if (idx + i >= board.gameboard.length) {
+          val y: Int = (idx + i - board.gameboard.length) % 14
           board.gameboard(y) += 1
         } else {
-          board.gameboard(index + i) += 1
+          board.gameboard(idx + i) += 1
+        }
+      }
+      if (i == countStonesInMuld) {
+        last = (idx + i) % 14
+      }
+    }
+    checkExtra(last)
+    //if (nochmal dran)
+    //  neuer zug round -= 1
+    print(last)
+    round += 1
+    //print(board.toString())
+  }
+  def checkExtra(last: Int): Unit ={
+    //println("checkExtra!")
+    if ((round % 2 == 1 && last == 0) || (round % 2 == 0 && last == 7)) {
+      //print("New Turn")
+      //tui.startTurn()
+      // TODO: new Turn
+      round -= 1
+    }
+    if (board.gameboard(last) == 1) {
+      var own = false
+      if ((1 <= last) && (last <= 6) && round % 2 == 0)
+        own = true
+      if ((8 <= last) && (last <= 13) && round % 2 == 1)
+        own = true
+      //println("own= " + own)
+      if (own) {
+        val idx = 14 - last
+        if (round % 2 == 0){
+          board.gameboard(0) += board.gameboard(idx)
+          board.gameboard(idx) = 0
+        } else {
+          board.gameboard(0) += board.gameboard(idx)
+          board.gameboard(idx) = 0
         }
       }
     }
-    round += 1
-    //print(board.toString())
   }
 
   def undo: Unit = {
@@ -63,25 +100,32 @@ class Controller {
 
   def win: Int = {
     var x: Int = 0
-    for (i <- 0 until board.stones) {
-      x += board.gameboard(i + 1)
-      print(x)
+    for (i <- 1 until 6 + 1) {
+      x += board.gameboard(i)
     }
     var y: Int = 0
-    for (i <- 0 until board.stones)
-      y += board.gameboard(i + board.stones + 1)
+    for (i <- 1 until 6 + 1)
+      y += board.gameboard(i + 7)
+    
+    board.gameboard(7) += x
+    board.gameboard(0) += y
 
-    if (x == 0) {
+    if (board.gameboard(7) > board.gameboard(0)) {
+      println("P1: " + board.gameboard(7) + " P2: " + board.gameboard(0))
+      println("WIN PLAYER 1")
       1
-    } else if (y == 0) {
+    } else if (board.gameboard(0) > board.gameboard(7)) {
+      println("P1: " + board.gameboard(7) + " P2: " + board.gameboard(0))
+      println("WIN PLAYER 2")
       2
     } else {
+      print("TIE")
       0
     }
   }
 
   def setWinp1: Unit = {
-    var winboard: Array[Int] = Array[Int](0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6)
-    board.gameboard = winboard.clone
+    val WinBoard: Array[Int] = Array[Int](0, 0, 0, 0, 0, 0, 0, 50, 6, 6, 6, 6, 6, 6)
+    board.gameboard = WinBoard.clone
   }
 }
