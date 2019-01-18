9bce8a937d2519a68c23843cae31c656a2bbfea2
sammyCatlady42 <sammy_isted@t-online.de>
2019-01-18 19:52:43 +0100

    chose 4 or 6 stones to start

--- src/main/scala/de/htwg/se/Kalaha/model/Gameboard.scala
+++ src/main/scala/de/htwg/se/Kalaha/model/Gameboard.scala
@@ -2,12 +2,17 @@ package de.htwg.se.Kalaha.model
 
 case class Gameboard() {
   val SIZE = 14
-  val stones = 6
+  var stones = 6
   var startboard : Array[Int] = Array[Int] (0, stones, stones, stones, stones, stones, stones, 0, stones, stones, stones, stones, stones, stones)
   var gameboard = new Array[Int] (SIZE)
   var oldgb = new Array[Int] (SIZE)
 
-  def boardInit: Unit = {
+  def boardInit(amountStonesStart : Int): Unit = {
+    stones = amountStonesStart
+    for(i <- 1 until SIZE){
+      print(gameboard(i))
+      if(startboard(i) != 0) startboard(i) = amountStonesStart
+    }
     gameboard = startboard.clone()
   }
 
