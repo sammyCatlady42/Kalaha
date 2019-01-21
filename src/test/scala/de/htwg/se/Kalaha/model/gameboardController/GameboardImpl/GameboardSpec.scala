48e34359437f939e22241eddc8ce159a1cfa3d9e
sammyCatlady42 <sammy_isted@t-online.de>
2019-01-21 04:13:19 +0100

    added toJson test

--- src/test/scala/de/htwg/se/Kalaha/model/gameboardController/GameboardImpl/GameboardSpec.scala
+++ src/test/scala/de/htwg/se/Kalaha/model/gameboardController/GameboardImpl/GameboardSpec.scala
@@ -1,9 +1,10 @@
-package scala.de.htwg.se.Kalaha.model
+package de.htwg.se.Kalaha.model.gameboardController.GameboardImpl
 
-import de.htwg.se.Kalaha.model.gameboardController.GameboardImpl.Gameboard
+import de.htwg.se.Kalaha.controller.controllerComponent.ControllerImpl.Controller
 import org.junit.runner.RunWith
-import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json.{JsNumber, JsValue, Json}
 
 @RunWith(classOf[JUnitRunner])
 class GameboardSpec extends WordSpec with Matchers {
@@ -29,22 +30,46 @@ class GameboardSpec extends WordSpec with Matchers {
       }
 
       "have empty space" in {
-        print("Feld " + 0 + "-- Inhalt " + board.startboard(0) + "\n")
         board.startboard(0) should be(0)
-        print("Feld " + 7 + "-- Inhalt " + board.startboard(7) + "\n")
         board.startboard(7) should be(0)
       }
       "have no field with 0 from 1 to 6" in {
         for (y <- 1 to 6) {
-          print("Feld " + y + "-- Inhalt " + board.startboard(y) + "\n")
           board.startboard(y) shouldNot be(0)
         }
       }
       "have no field with 0 from 8 to 13" in {
         for (y <- 1 to 6) {
-          print("Feld " + (y + 7) + "-- Inhalt " + board.startboard(y + 7) + "\n")
           board.startboard(y + 7) shouldNot be(0)
         }
+      }
+      "ToJson" in {
+        val controller = new Controller
+        val startFeld = "15000013000001200"
+        val startboard: Array[Int] = Array[Int](15, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0)
+        controller.board.setBoard(startboard)
+        val testJson =
+          Json.obj(
+            "gameboard" -> Json.obj(
+              "round" -> JsNumber(0),
+              "board" -> Json.arr(
+                Json.obj(
+                  "0" -> 15,
+                  "1" -> 2,
+                  "2" -> 0,
+                  "3" -> 0,
+                  "4" -> 0,
+                  "5" -> 0,
+                  "6" -> 0,
+                  "7" -> 2,
+                  "8" -> 0,
+                  "9" -> 0,
+                  "10" -> 0,
+                  "11" -> 1,
+                  "12" -> 0,
+                  "13" -> 0
+                ))))
+        controller.board.toJson should be(testJson)
       }
     }
   }
